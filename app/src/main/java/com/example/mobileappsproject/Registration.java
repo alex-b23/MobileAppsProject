9bcff835cb6b777158cbaab99dff6869ea83d17a
alex-b23 <138300434+alex-b23@users.noreply.github.com>
2023-11-14 14:38:00 +0000

    Added internet user permission + started working on -> Registration.java

--- app/src/main/java/com/example/mobileappsproject/Registration.java
+++ app/src/main/java/com/example/mobileappsproject/Registration.java
@@ -3,12 +3,22 @@ package com.example.mobileappsproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class Registration extends AppCompatActivity {
+
+    TextInputEditText userEmail;
+    TextInputEditText userPassword;
+    Button registrationBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        userEmail = findViewById(R.id.userEmail);
+        userPassword = findViewById(R.id.userPassword);
+        registrationBtn = findViewById(R.id.registrationBtn);
     }
 }
