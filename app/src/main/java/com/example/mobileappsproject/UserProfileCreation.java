aa657462e149abf36af6dc1701df120bcee1404d
alex-b23 <138300434+alex-b23@users.noreply.github.com>
2023-11-21 10:16:49 +0000

    Created Initial Layout for Create Profile -> Proper Commit

---
+++ app/src/main/java/com/example/mobileappsproject/UserProfileCreation.java
@@ -0,0 +1,14 @@
+package com.example.mobileappsproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserProfileCreation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile_creation);
+    }
+}
