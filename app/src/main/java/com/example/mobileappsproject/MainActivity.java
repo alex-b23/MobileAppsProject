74f1dbda38507a15673fcadc50ed0771f7a9859e
alex-b23 <138300434+alex-b23@users.noreply.github.com>
2023-11-22 09:30:00 +0000

    Added Sign out Button to main for testing

--- app/src/main/java/com/example/mobileappsproject/MainActivity.java
+++ app/src/main/java/com/example/mobileappsproject/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.mobileappsproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button signOut;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        signOut = findViewById(R.id.signOut);
+
+        signOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Sign out the user
+                mAuth.signOut();
+
+                // Redirect to the Login activity
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
