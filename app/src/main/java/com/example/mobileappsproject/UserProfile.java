a1b3a2a679674f92bfffc9fb7b73a0f1506a507a
alex-b23 <138300434+alex-b23@users.noreply.github.com>
2023-11-29 12:43:21 +0000

    Updated Layout and fetching users details from the database based on the uid

--- app/src/main/java/com/example/mobileappsproject/UserProfile.java
+++ app/src/main/java/com/example/mobileappsproject/UserProfile.java
@@ -1,42 +1,36 @@
 package com.example.mobileappsproject;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link UserProfile#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class UserProfile extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private TextView nameTextView;
+    private TextView usernameTextView;
+    private TextView bioTextView;
+    private FirebaseFirestore db;
 
     public UserProfile() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment UserProfile.
-     */
-    // TODO: Rename and change types and number of parameters
     public static UserProfile newInstance(String param1, String param2) {
         UserProfile fragment = new UserProfile();
         Bundle args = new Bundle();
@@ -58,7 +52,34 @@ public class UserProfile extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_user_profile, container, false);
+
+        nameTextView = view.findViewById(R.id.name);
+        usernameTextView = view.findViewById(R.id.username);
+        bioTextView = view.findViewById(R.id.bio);
+
+        db = FirebaseFirestore.getInstance();
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Get the UID of the current user
+        String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        // Fetch the document with the user's UID
+        db.collection("users").document(uid).get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                DocumentSnapshot document = task.getResult();
+                if (document.exists()) {
+                    nameTextView.setText(document.getString("DisplayName"));
+                    usernameTextView.setText(document.getString("Username"));
+                    bioTextView.setText(document.getString("Bio"));
+                }
+            }
+        });
     }
 }
